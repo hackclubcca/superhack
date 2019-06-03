0cd9079e0e381577b0788d9f7842a1d86bf09882
Harris Beg <legostarwarsguy02@gmail.com>
2019-06-02 21:57:03 -0700

    Better styling

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
-
+from flask_wtf import Form
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
+
+"""
+
+
+Add a contact form
+
+
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
