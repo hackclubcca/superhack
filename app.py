533222d6c8d0aae161db7da8626c8a7f95209f28
Harris Beg <legostarwarsguy02@gmail.com>
2019-06-03 22:03:13 -0700

    Major functional changes + SCSS

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 from wtforms import Form
+from flask_assets import Environment, Bundle
 app = Flask(__name__)
+assets = Environment(app)
+scss = Bundle('scss/index.scss',
+            filters='scss', output='gen/all.css')
+assets.register('scss', scss)
 
 
 @app.route('/')
