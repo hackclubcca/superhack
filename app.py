31f4358e0d8b533b0ad45fb151c597f63adec3f0
Harris Beg <harris@harrisbeg.com>
2019-08-18 17:47:50 -0700

    Brighter colours

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, make_response
 from wtforms import Form
 from flask_assets import Environment, Bundle
+
 app = Flask(__name__)
 assets = Environment(app)
 scss = Bundle('scss/index.scss',
-            filters='scss', output='gen/all.css')
+              filters='scss', output='gen/all.css')
 assets.register('scss', scss)
 
 
@@ -12,9 +13,11 @@ assets.register('scss', scss)
 def index():
     return render_template("index.html")
 
+
 @app.route('/document/<name>')
 def document(name):
     return render_template("document.html", document_name=name)
+
 
 """
 
