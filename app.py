b6ef3e4d56136affbe8df980194247d8890cd9a0
Danial Beg <danny00b@gmail.com>
2019-10-29 17:41:31 -0700

    More professional

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, make_response, request
-from wtforms import Form
+from flask_sitemap import Sitemap
 from flask_assets import Environment, Bundle
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+smp = Sitemap(app=app)
 assets = Environment(app)
 scss = Bundle('scss/index.scss',
               filters='scss', output='gen/all.css')
 assets.register('scss', scss)
 
 STATIC_URL = "/static/"
+
+
 @app.route('/')
 def index():
     if not request.headers.get("USER_AGENT"):
